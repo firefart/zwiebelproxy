21f33cec189adf82503ba40c613034f6d0dd3f6f
Christian Mehlmauer <firefart@gmail.com>
2022-06-17 17:47:28 +0200

    proxy

--- proxy.go
+++ proxy.go
@@ -9,20 +9,9 @@ import (
 )
 
 func (app *application) modifyResponse(resp *http.Response) error {
-	app.logger.Debug("entered modifyResponse")
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("error on reading body: %w", err)
-	}
-
-	app.logger.Debugf("%s: Got a %d body len with a status of %d", resp.Request.URL.String(), len(body), resp.StatusCode)
-
-	// replace stuff for domain replacement
-	body = bytes.ReplaceAll(body, []byte(`.onion/`), []byte(fmt.Sprintf(`%s/`, app.domain)))
-	body = bytes.ReplaceAll(body, []byte(`.onion"`), []byte(fmt.Sprintf(`%s"`, app.domain)))
+	app.logger.Debug("entered modifyResponse for %s with status %d", resp.Request.URL.String(), resp.StatusCode)
 
 	app.logger.Debugf("Header: %#v", resp.Header)
-
 	for k, v := range resp.Header {
 		k = strings.ReplaceAll(k, ".onion", app.domain)
 		resp.Header[k] = []string{}
@@ -32,9 +21,24 @@ func (app *application) modifyResponse(resp *http.Response) error {
 		}
 	}
 
-	app.logger.Debugf("modified body")
+	// no body modification on file downloads
+	contentDisp, ok := resp.Header["Content-Disposition"]
+	if ok && len(contentDisp) > 0 && strings.HasPrefix(contentDisp[0], "attachment") {
+		return nil
+	}
 
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("error on reading body: %w", err)
+	}
+	app.logger.Debugf("%s: Got a %d body len", resp.Request.URL.String(), len(body))
+	// replace stuff for domain replacement
+	body = bytes.ReplaceAll(body, []byte(`.onion/`), []byte(fmt.Sprintf(`%s/`, app.domain)))
+	body = bytes.ReplaceAll(body, []byte(`.onion"`), []byte(fmt.Sprintf(`%s"`, app.domain)))
+
+	// body can be read only once so recreate a new reader
 	resp.Body = io.NopCloser(bytes.NewBuffer(body))
+	// update the content-length to our new body
 	resp.Header["Content-Length"] = []string{fmt.Sprint(len(body))}
 	return nil
 }
