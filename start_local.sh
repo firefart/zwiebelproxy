33a728415f8d4b80305f2c9b79c84aaace31dca0
Christian Mehlmauer <105281+firefart@users.noreply.github.com>
2023-10-10 14:13:57 +0200

    local

---
+++ start_local.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+docker pull golang:latest
+docker pull alpine:latest
+docker compose -f docker-compose.local.yml pull
+docker compose -f docker-compose.local.yml build
+docker compose -f docker-compose.local.yml stop
+docker compose -f docker-compose.local.yml rm -f -v -s
+docker compose -f docker-compose.local.yml up -d --remove-orphans
+docker image prune -f
